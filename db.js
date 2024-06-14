febd5191313deb692160e9995e1c56c80569573e
Ruben <rubo.lopez.1999@alumnos.cei.es>
2024-06-14 12:29:54 +0200

    api todo react terminada

---
+++ db.js
@@ -0,0 +1,103 @@
+require("dotenv").config();
+
+const {MongoClient, ObjectId} = require("mongodb");
+
+function conectar(){
+    return MongoClient.connect(process.env.URL_MONGO)
+}
+
+function tareas(){
+    return new Promise(async (ok,ko) => {
+        try{
+            const conexion = await conectar();
+
+            let tareas = await conexion.db("tareas").collection("tareas").find({}).toArray();
+
+            conexion.close();
+
+            ok(tareas.map(({_id,tarea,terminada}) => {
+                return {id:_id,tarea,terminada}
+            }));
+
+        }catch(error){
+            ko({error : "error en el servidor"});
+        }
+        
+    })
+}
+
+function crearTarea(tarea){
+    return new Promise(async (ok,ko) => {
+        try{
+            const conexion = await conectar();
+
+            let {insertedId} = await conexion.db("tareas").collection("tareas").insertOne({tarea,terminada : false});
+
+            conexion.close();
+
+            ok(insertedId);
+
+        }catch(error){
+            ko({error : "error en el servidor"});
+        }
+        
+    })
+}
+
+function borrarTarea(id){
+    return new Promise(async (ok,ko) => {
+        try{
+            const conexion = await conectar();
+
+            let {deletedCount} = await conexion.db("tareas").collection("tareas").deleteOne({_id : new ObjectId(id)});
+
+            conexion.close();
+
+            ok(deletedCount);
+
+        }catch(error){
+            ko({error : "error en el servidor"});
+        }
+        
+    })
+}
+
+function toggleEstado(id){
+    return new Promise(async (ok,ko) => {
+        try{
+            const conexion = await conectar();
+
+            let {terminada}  = await conexion.db("tareas").collection("tareas").findOne({_id : new ObjectId(id)});
+
+            let {modifiedCount} = await conexion.db("tareas").collection("tareas").updateOne({_id : new ObjectId(id)}, {$set : {terminada : !terminada}});
+
+            conexion.close();
+
+            ok(modifiedCount);
+
+        }catch(error){
+            ko({error : "error en el servidor"});
+        }
+        
+    })
+}
+
+function editarTexto(id,tarea){
+    return new Promise(async (ok,ko) => {
+        try{
+            const conexion = await conectar();
+
+            let {modifiedCount} = await conexion.db("tareas").collection("tareas").updateOne({_id : new ObjectId(id)}, {$set : {tarea : tarea}});
+
+            conexion.close();
+
+            ok(modifiedCount);
+
+        }catch(error){
+            ko({error : "error en el servidor"});
+        }
+        
+    })
+}
+
+module.exports = {tareas, crearTarea, borrarTarea, toggleEstado, editarTexto};
---
+++ index.js
@@ -0,0 +1,89 @@
+require("dotenv").config();
+
+const {tareas, crearTarea, borrarTarea, toggleEstado,editarTexto} = require("./db");
+const cors = require("cors");
+const express = require("express");
+const {json} = require("body-parser");
+const servidor = express();
+
+
+servidor.use(cors());
+servidor.use(json());
+
+if(process.env.LOCAL){
+    servidor.use(express.static("./pruebas"))
+}
+
+servidor.get("/tareas", async (peticion,respuesta) => {
+    try{
+        let resultado = await tareas();
+
+        respuesta.json(resultado);
+    }catch(error){
+        respuesta.status(500);
+
+        respuesta.json(error);
+    }
+})
+
+servidor.post("/tareas/nueva", async (peticion,respuesta,siguiente) => {
+    if(!peticion.body.tarea || peticion.body.tarea.trim() == "" ){
+        return siguiente(true);
+    }
+    try{
+
+        let id = await  crearTarea(peticion.body.tarea);
+
+        respuesta.json({id});
+
+    }catch(error){
+        respuesta.status(500);
+
+        respuesta.json(error);
+    }
+})
+
+servidor.delete("/tareas/borrar/:id([0-9a-f]{24})", async (peticion,respuesta) => {
+    try{
+        let count = await borrarTarea(peticion.params.id);
+
+        respuesta.json({resultado : count ? "ok" : "ko"});
+    }catch(error){
+        respuesta.status(500);
+
+        respuesta.json(error);
+    }
+})
+
+servidor.put("/tareas/actualizar/:id([0-9a-f]{24})/:operacion(1|2)", async (peticion,respuesta,siguiente) => {
+    let operacion = Number(peticion.params.operacion);
+
+    let funciones = [editarTexto, toggleEstado];
+
+    if(operacion == 1 && (!peticion.body.tarea || peticion.body.tarea.trim() == "")){
+        return siguiente(true);
+    }
+    
+    try{
+        let count = await funciones[operacion - 1](peticion.params.id, operacion == 1 ? peticion.body.tarea : null );
+
+        respuesta.json({resultado : count ? "ok" : "ko"});
+    }catch(error){
+        respuesta.status(500);
+
+        respuesta.json(error); 
+    }
+})
+
+servidor.use((peticion,respuesta) => {
+    respuesta.status(404);
+    respuesta.json({error : "recurso no encontrado"});
+})
+
+servidor.use((error,peticion,respuesta,siguiente) => {
+    respuesta.status(400);
+    respuesta.json({error:"error en la petición"});
+})
+
+
+servidor.listen(process.env.PORT);
